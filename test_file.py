d9d381f861e74a5aa898c08504d22d0d63b2fe80
Antonio Lechuga <antoniolechugarivera@gmail.com>
2019-12-09 14:19:20 -0600

    Minor changes to test network class implementation

--- test_file.py
+++ test_file.py
@@ -3,14 +3,19 @@ from lib.classes.stream import Stream
 from lib.classes.utility import Utility
 from lib.classes.minimum_utility_problem import Min_Utility_Problem
 from lib.solvers.min_utility_solver import solve_min_uility_instace
+from lib.classes.network import Network
 
 
 if __name__ == '__main__':
 
     problems = ["balanced5", "balanced8", "balanced10", "balanced12", "balanced15"]
     problems += ["unbalanced5", "unbalanced10", "unbalanced15", "unbalanced17", "unbalanced20"]
+
+    # problems = ["balanced5"]
     
     for problem in problems:
         print("-------------------------------------{}-------------------------------------".format(problem))
         minup = Min_Utility_Problem.generate_from_data(problem)
-        solve_min_uility_instace(minup)
+        (sigma_HU, delta_HU) = solve_min_uility_instace(minup)
+        network = Network(minup, sigma_HU, delta_HU)
+        print(network)
